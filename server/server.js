fa3d23d9b26c2837b42ab3ab594afd49a0fdd482
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-09-14 14:57:58 -0700

    removed graphql, just using apollo-server and apollo sandbox now

--- server/server.js
+++ server/server.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer} = require('apollo-server');
 
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers
-}, async () => {
-    await server.applyMiddleware({ app });
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// server.applyMiddleware({ app });
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 
 db.once('open', () => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`API server running on port ${PORT}`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
 })
