258942561dc08357178404744510418b7544b923
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-09-14 10:16:52 -0700

    initial server set up

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/shamazon', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
