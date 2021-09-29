ed9c4e4157f1a6d61bf201bc71d7eb74154cc737
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-09-29 10:33:33 -0700

    finally added bootstrap lol

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Header from './components/Header';
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,9 @@
-import 'bootstrap';
+
 const Header = () => {
     return(
         <header className = "row">
             <div className="col">shamazon.</div>
+            <div className = 'col'>hi</div>
             <div className="col">cart</div>
         </header>
     );
